4733834383303fbd71f13d477e31cd1505e8d277
Klaus <whynotmemylove99@gmail.com>
2023-07-20 22:57:14 +0700

    complete half of week 3

--- Week 3 - Navigation, Updating and Assets in React.js/1.demos/1.the-navbar-demo/src/App.js
+++ Week 3 - Navigation, Updating and Assets in React.js/1.demos/1.the-navbar-demo/src/App.js
@@ -1,17 +1,24 @@
 import Homepage from './Homepage';
 import AboutMe from './AboutMe';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';   
+// 1. import the Routes, Route, Link components from react-router-dom
 
 function App() {
   return (
     <div className="App">
-      <nav className="nav">
-        <Link to="/" className="nav-item">Homepage</Link>
+      {/*2. create a nav element with the class of nav*/}
+      <nav className="nav">  
+        {/*// 3. use the Link component to create a link to the homepage} */}
+        <Link to="/" className="nav-item">Homepage</Link> 
         <Link to="/about-me" className="nav-item">About Me</Link>
       </nav>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
+
+      {/*4. use the Routes component to create the routes for the homepage and about me page*/}
+      <Routes> 
+        {/*5. use the Route component to create the route for the homepage and about me page*/}
+        <Route path="/" element={<Homepage />} /> 
         <Route path="/about-me" element={<AboutMe />} />
+        {/* path need to same name with the link created */}
       </Routes>
     </div>
   );
--- Week 3 - Navigation, Updating and Assets in React.js/1.demos/2.single-view-conditional-updates-demo/src/App.js
+++ Week 3 - Navigation, Updating and Assets in React.js/1.demos/2.single-view-conditional-updates-demo/src/App.js
@@ -1,11 +1,11 @@
 function App() {
   const time = new Date();
   const day = time.toLocaleString("en-US", { weekday: "long" });
-  const morning = time.getHours() >= 6 && time.getHours() <= 12;
+  const morning = time.getHours() >= 6 && time.getHours() <= 23;
   let dayMessage;
 
   if (day.toLowerCase() === "monday") {
-    dayMessage = `Happy ${day}`;
+    dayMessage = `Happy ${day}`; // $ is used to insert a variable into a string
   } else if (day.toLowerCase() === "tuesday") {
     dayMessage = `${day}, four days to go`;
   } else if (day.toLowerCase() === "wednesday") {
