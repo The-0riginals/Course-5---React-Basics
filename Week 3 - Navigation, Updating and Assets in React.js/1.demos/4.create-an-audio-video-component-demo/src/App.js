4909bc32bce570b6c54a7afe9d1568c0676d4670
Klaus <whynotmemylove99@gmail.com>
2023-07-21 15:54:05 +0700

    complete week 3

--- Week 3 - Navigation, Updating and Assets in React.js/1.demos/4.create-an-audio-video-component-demo/src/App.js
+++ Week 3 - Navigation, Updating and Assets in React.js/1.demos/4.create-an-audio-video-component-demo/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import rooftops from './assets/images/central-park.jpg';
 import ReactPlayer from "react-player";
 
 function App() {
-  const vidUrl = "https://fb.watch/hP6j60tdjm/";
+  const vidUrl = "https://www.youtube.com/watch?v=RzhAS_GnJIc";
 
   return (
     <div className='App'>
+      <div>
+      <h1>
+        Task: add three images with some styling
+      </h1>
+        <img height={200} src={rooftops} alt="An image of a rooftop in New York" />
+        <img height={200} src={require("./assets/images/central-park.jpg")} alt="An image of central park" />
+        <img height={200} src="https://picsum.photos/id/400/265" alt="An image of a random pic" />
+      
+      </div>
+
       <h1>React Player example</h1>
       <ReactPlayer
+        // style={{ textAlign: "auto" }}
         url={vidUrl}
         playing={false}
         volume={0.5}
---
+++ Week 3 - Navigation, Updating and Assets in React.js/4.labs/2.displaying-images/src/App.js
@@ -0,0 +1,13 @@
+import logo from './assets/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task: Add three images below</h1>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
